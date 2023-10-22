1c6f3a6273ba6e8c386e437b3169e7d1af434114
Tobin Baker <tobin.d.baker@gmail.com>
2023-10-22 08:06:01 -0700

    Initial commit.

---
+++ atomik/assets/js/typewriter-custom.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* ========= Typewriter effect =========== */
+/* Ref: https://safi.me.uk/typewriterjs/ */
+const instance = new Typewriter('#typewriter', {
+  strings: ['Low-Latency Desktop Apps', 'High-Performance Web Servers', 'Database Engines', 'Games', 'Robotics'],
+  autoStart: true,
+  loop: true,
+});
